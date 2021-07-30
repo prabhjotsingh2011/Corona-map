b8a007b0f261704f4c08a14684713e1712d8cec5
Prabhjot Singh <psingh20112002@gmail.com>
2021-07-31 02:58:38 +0530

    CoronaMap

---
+++ index.js
@@ -0,0 +1,29 @@
+function updateMap()
+ {
+    fetch("/data.json")
+    // fetch("/data.json")
+    .then(response => response.json())
+    .then(rsp => {
+        console.log(rsp.data)
+        rsp.data.forEach(element => {
+            latitude=element.latitude
+            longitude=element.longitude;
+
+            cases=element.infected;
+            if (cases>255) {
+                color="red"
+            }
+            else{
+                color=`rgb(${color},0,0)`
+            }
+
+            new mapboxgl.Marker({
+                draggable: false,
+                color: color
+            })
+                .setLngLat([longitude,latitude])
+                .addTo(map)
+        });
+    })
+}
+updateMap()
